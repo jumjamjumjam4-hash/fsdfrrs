0beb2d4cd6a6336447e3ef400aaf53cd523873b9
jumjamjumjam4-hash <jumjamjumjam4@gmail.com>
2026-09-18 02:21:04 -0700

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -14,7 +14,7 @@ export async function onRequest(context) {
     <title>Welcome</title>
     <meta property="og:title" content="😍🎧🎬">
     <meta property="og:description" content="">
-    <meta property="og:image" content="https://media.istockphoto.com/id/493957429/photo/peacock-flowers.jpg?s=1024x1024&w=is&k=20&c=ZcWAJi0qI6WZiqKdbpKUEO9GQFoIMBDny66xg8Pc9w4=">
+    <meta property="og:image" content="https://i.pinimg.com/originals/18/52/4f/18524f9ef9c9f5cf89d375ad40c8b50d.png">
     <meta property="og:url" content="https://www.google.com">
     <meta property="og:type" content="website">
 </head>
